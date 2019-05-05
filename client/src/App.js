212638a8fbe758c329fe4a5a137f512e68e6281b
SONGHYUN KIM <bridge00087@gmail.com>
2019-05-05 19:09:31 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -20,34 +20,24 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/any',
-  'name': '山田　太郎',
-  'birthday': '961222',
-  'gender': '男',
-  'job': '大学生'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/any',
-  'name': '鈴木　二郎',
-  'birthday': '970521',
-  'gender': '男',
-  'job': '会社員'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/any',
-  'name': '宮崎　優子',
-  'birthday': '890801',
-  'gender': '女',
-  'job': '会社員'
-}
-]
-
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -64,7 +54,9 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> ); })}
+            {this.state.customers ? this.state.customers.map(c => { 
+              return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> ); 
+            }) : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -11,4 +11,34 @@ app.get('/api/hello', (req, res) => {
     res.send({message: 'こんにちはExpress!'});
 });
 
+// 顧客リストの取得
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+          'id': 1,
+          'image': 'https://placeimg.com/64/64/any',
+          'name': '山田　太郎',
+          'birthday': '961222',
+          'gender': '男',
+          'job': '大学生'
+        },
+        {
+          'id': 2,
+          'image': 'https://placeimg.com/64/64/any',
+          'name': '鈴木　二郎',
+          'birthday': '970521',
+          'gender': '男',
+          'job': '会社員'
+        },
+        {
+          'id': 3,
+          'image': 'https://placeimg.com/64/64/any',
+          'name': '宮崎　優子',
+          'birthday': '890801',
+          'gender': '女',
+          'job': '会社員'
+        }
+        ])
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
